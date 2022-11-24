d27f092b7659a5e03595df6fc1b1dbe3da1fa160
kumkumshukla631@gmail.com <kumkumshukla631@gmail.com>
2022-11-24 22:13:30 +0530

    intial commit

---
+++ Variables_Comments.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"hello world"<<endl;
+    //this is a single line comment
+    /*this
+    is
+    a 
+    multi 
+    line
+    comment*/
+    int sum = 25;
+    cout<<sum << endl;
+    return 0;
+}
